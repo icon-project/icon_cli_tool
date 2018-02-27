80072ebc77d6b218062ee152b3dd9c7e56e03028
Boyeon.Choi <boyeon.choi@icon.foundation>
2018-02-27 16:25:59 +0900

    Add unittest template

---
+++ tests/test_api.py
@@ -0,0 +1,14 @@
+import unittest
+
+
+class TestAPI(unittest.TestCase):
+    """
+    Test that execute the api about Wallet
+    """
+
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+
+if __name__ == "__main__":
+    unittest.main()
