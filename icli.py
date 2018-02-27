f721564c22f253530ffa06473dc315c195f9c94f
김인원 <inwonkim@icon.foundation>
2018-02-27 11:50:12 +0900

    edit command format

--- icli.py
+++ icli.py
@@ -1,17 +1,15 @@
 import argparse
-import sys
 
 
-def main(argv):
-    parser = argparse.ArgumentParser(description="test")
-    parser = argparse.ArgumentParser(prog='icli.py', usage='''
+def main():
+    parser = argparse.ArgumentParser(description="test", prog='icli.py', usage='''
     %(prog)s command options
     Normal commands:
       version
       help
 
     Wallet Commands:
-      wallet create <file path> -p <password>
+      wallet create <wallet name> <file path> -p <password>
       wallet show <file path> -p <password>
       asset list <file path> -p <password>
       transfer  <to> <amount> <file path> -p <password> -f <fee> -d <decimal point=18>
@@ -27,7 +25,8 @@ def main(argv):
     command = ' '.join(args.command[:2])
 
     if command == 'wallet create':
-        create_wallet(args.password)
+        # print("wallet name: ", args.command[2])
+        create_wallet("kim", "./keystore/", args.password)
     elif command == 'wallet show':
         show_wallet(args.password)
     elif command == 'asset list':
@@ -36,12 +35,21 @@ def main(argv):
         transfer(*args.command, password=args.password, fee=args.fee, decimal_point=args.decimal_point)
     elif command.split(' ')[0] == 'version':
         print("version : 0.0.1")
+    elif command.split(' ')[0] == 'help':
+        parser.print_help()
     else:
         print(f"Unsupported command {args.command[0]}")
         parser.print_help()
 
 
-def create_wallet(password):
+def create_wallet(wallet_name, file_path, password):
+    """
+    create wallet file with given wallet name, password and file path.
+    :param wallet_name: name for wallet
+    :param file_path: File path for the keystore file of the wallet
+    :param password: password including alphabet character, number, and special character.
+    :return:
+    """
     print(password)
 
 
@@ -61,8 +69,13 @@ def transfer(*commands, password=None, fee=None, decimal_point=None):
         print(commands[0], commands[1], commands[2], commands[3], password, fee, decimal_point)
 
 
+def check_command_format(*command, correct_length):
+
+    return True
+
+
 if __name__ == "__main__":
     try:
-        main(sys.argv[1:])
+        main()
     except KeyboardInterrupt:
         print("exit")
